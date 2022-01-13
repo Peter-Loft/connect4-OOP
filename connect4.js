f08cb6fef0518bd3214f57a8ce1eb1e886504cb7
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-13 14:02:40 -0600

    created Game Class

--- connect4.js
+++ connect4.js
@@ -11,15 +11,34 @@ const HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
+class Game {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+    let board = [];
+  }
+
+  makeBoard() {
+    for (let y = 0; y < this.height; y++) {
+      board.push(Array.from({ length: this.width }));
+    }
+  }
+  
+  
+}
+
+
+
 /** makeBoard: create in-JS board structure:
  *   board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
-  for (let y = 0; y < HEIGHT; y++) {
-    board.push(Array.from({ length: WIDTH }));
-  }
-}
+// 
+// function makeBoard() {
+//   for (let y = 0; y < HEIGHT; y++) {
+//     board.push(Array.from({ length: WIDTH }));
+//   }
+// }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
